d00ccfaadf4c6d747dbf50c71294d484865b330e
Ryan Spear <rspear@oucs1357.otago.ac.nz>
2017-12-07 14:20:49 +1300

    instead of creating an output image, creates a matrix where each pixel is given a number

--- pleaseWork/main.cpp
+++ pleaseWork/main.cpp
@@ -10,6 +10,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <ctime>
 
 using namespace cv;
 using namespace std;
@@ -29,31 +30,46 @@ Vec3b randomPixel(){
  that colour will be used in those positions in the output image.
  */
 int main(int argv, char** argc) {
+    
+    std::clock_t start;
+    double duration;
+    
+    start = std::clock();
+    
+    
     int count = 0; // tracks how many times a pixel in the right is the same as the left.
     double eyeWidth = 4.5*37.795276; // how many pixels the typical eyes are apart
-    Vec3b defaultPix = {220, 100, 50};
     
-    
-    Mat left = imread("carLeft.jpg" , CV_LOAD_IMAGE_UNCHANGED);
-    Mat right = imread("carRight.jpg" , CV_LOAD_IMAGE_UNCHANGED);
+    Mat left = imread("faceLeft.jpg" , CV_LOAD_IMAGE_UNCHANGED);
+    Mat right = imread("faceRight.jpg" , CV_LOAD_IMAGE_UNCHANGED);
     Mat output = Mat::zeros(left.size(), CV_8UC3); // fills an empty output with black pixels
+    int numberMatrix[left.rows][left.cols];
+    int numMatIndex = 0;
+    int numMatCurrent = numMatIndex;
     
     if (left.empty() || right.empty()){
         cout << "image(s) failed to load"; // if images aren't in the folder or named wrong
     }
     
-    /* for loops iterating though ever pixel and getting the RGB values. */
+    /* for loops iterating though every pixel and getting the RGB values. */
     for(int rows = 0; rows < left.rows; rows++){
-        bool visited[left.cols];
-        for(int y = 0; y < left.cols; y++){
-            visited[y] = 0;
-        }
+        
         for(int cols = 0; cols < left.cols; cols++){
             Vec3b leftIntensity = left.at<Vec3b>(rows,cols);
             float B = leftIntensity.val[0]/10; // blue, green & red values
             float G = leftIntensity.val[1]/10; // divided by 10 so colour doesn't have to be exactly the same
             float R = leftIntensity.val[2]/10; // just close enough
-            int i = cols - 50; //this gives the effect of having the right image start further to the right
+            int i = cols; // images start in same position
+            
+            /* deals with left pixel value. if not 0, keep using that value, otherwise give it a new value */
+            if(numberMatrix[rows][cols] != 0){
+                numMatCurrent = numberMatrix[rows][cols];
+            } else {
+                numMatIndex++;
+                numMatCurrent = numMatIndex;
+                numberMatrix[rows][cols] = numMatIndex;
+            }
+            
             
             /* goes along the row until eye width apart comparing each pixel in the right image to the original pixel in the left image */
             while(i < cols + eyeWidth && i < left.cols){
@@ -62,20 +78,44 @@ int main(int argv, char** argc) {
                 float Gcompare = (rightIntensity.val[1])/10;
                 float Rcompare = (rightIntensity.val[2])/10;
                 
-                if(B == Bcompare && G == Gcompare && R == Rcompare){ // if they're close enough (divided by 10)
+                
+                if(B == Bcompare && G == Gcompare && R == Rcompare){ // if the pixels are the same(ish)
                     count++;
-                    output.at<Vec3b>(rows,i) = leftIntensity; // output pixel same colour as left pixel
-                    visited[i] = true;
-                } else if(!visited[i]){
+                    /* if pixel hasn't been given a value yet then give it the current value.
+                       if pixel has a value that is different, replace every pixel with that value (including the current pixel) with the current value */
+                    if(numberMatrix[rows][i] == 0){
+                        numberMatrix[rows][i] = numMatCurrent;
+                    } else {
+                        if(numberMatrix[rows][i] != numMatCurrent){
+                            int change = numberMatrix[rows][i];
+                            for(int j = 0; j < left.cols; j++){
+                                if(numberMatrix[rows][j] == change){
+                                    numberMatrix[rows][j] = numMatCurrent;
+                                }
+                            }
+                        }
+                    }
+                }
+                    //output.at<Vec3b>(rows,i) = leftIntensity; // output pixel same colour as left pixel
+                    
+                /*} else if(!visited[i]){
                     output.at<Vec3b>(rows,i) = randomPixel(); // if they're not the same make it a random colour
-                } /* Problem: Doing this will overwrite previously visited pixels that were the same. Do pixels need
+                }*/ /* Problem: Doing this will overwrite previously visited pixels that were the same. Do pixels need
                    a "visited" variable to test whether they've already been made default pix colour.*/
                 i++;
             }
         }
     }
     /* Deals with showing the images and their position on the monitor */
+    
+    for(int rows = 0; rows < left.rows; rows++){
+        cout << numberMatrix[rows][1] << "\n";
+    }
+    
     cout << "Count: " << count;
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    
+    std::cout<<"printf: "<< duration <<'\n';
     namedWindow("Output", WINDOW_AUTOSIZE);
     moveWindow("Output", 850, 100);
     namedWindow("Left", WINDOW_AUTOSIZE);
@@ -89,6 +129,7 @@ int main(int argv, char** argc) {
     
     
 }
+
 
 
 
