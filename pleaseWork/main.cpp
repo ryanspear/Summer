1bae74d29fd4264d80e71dbb5a3428649059dd8a
Ryan Spear <rspear@oucs1357.otago.ac.nz>
2017-12-12 15:12:54 +1300

    - Seed happens at start of program to get new random color each time. - Coloring the output image is done by using a numbered matrix. Matrix gives pixels that are the same color in L&R the same number. - Loops through the numbered matrix and gives pixels with same number the same RANDOM colored pixel.

--- pleaseWork/main.cpp
+++ pleaseWork/main.cpp
@@ -15,14 +15,17 @@
 using namespace cv;
 using namespace std;
 
+void seed()
+{
+    srand(time(0));
+}
+
 uchar randomPixelGrey(){
-    srand(time(NULL));
     uchar gPixel = rand() % 256;
     return gPixel;
 }
 /* returns a psudo-random pixel based on time */
 Vec3b randomPixelColour(){
-    srand(time(NULL));
     Vec3b rPixel;
     int B = rand() % 256;
     int G = rand() % 256;
@@ -30,23 +33,6 @@ Vec3b randomPixelColour(){
     
     rPixel = {static_cast<unsigned char>(B), static_cast<unsigned char>(G), static_cast<unsigned char>(R)};
     return rPixel;
-}
-/* returns a seeded RGB pixel */
-Vec3b seedPixelColour(int seed){
-    srand(seed);
-    Vec3b rPixel;
-    int B = rand() % 256;
-    int G = rand() % 256;
-    int R = rand() % 256;
-    
-    rPixel = {static_cast<unsigned char>(B), static_cast<unsigned char>(G), static_cast<unsigned char>(R)};
-    return rPixel;
-}
-/* returns a seeded greyscale pixel */
-uchar seedPixelGrey(int seed){
-    srand(seed);
-    uchar gPixel = rand() % 256;
-    return gPixel;
 }
 
 /* creates an output image comparing pixels in the right image to the left image.
@@ -54,20 +40,24 @@ uchar seedPixelGrey(int seed){
  that colour will be used in those positions in the output image.
  */
 int main(int argv, char** argc) {
-    
+    /* timing the program */
     std::clock_t start;
     double duration;
+    start = std::clock();
     
-    start = std::clock(); // timing the program.
-    
-    
+    int bucketSize = 3;
     int count = 0; // tracks how many times a pixel in the right is the same as the left.
     double eyeWidth = 241; // how many pixels the typical eyes are apart
     
     Mat left = imread("FountainLeft.jpg" , CV_LOAD_IMAGE_UNCHANGED);
     Mat right = imread("FountainRight.jpg" , CV_LOAD_IMAGE_UNCHANGED);
-    Mat output = Mat::zeros(left.size(), CV_8UC3); // fills an empty output with black pixels
+    Mat output = Mat::zeros(left.size(), CV_8UC1); // fills an empty output with black pixels
     int numberMatrix[output.rows][output.cols]; // holds number representation of pixels that should be same colour
+    for(int i = 0; i < output.rows; i++){
+        for(int j = 0; j < output.cols; j++){
+            numberMatrix[i][j] = 0;
+        }
+    }
     int numMatIndex = 0; // gives a new number to pixels that are a new colour
     int numMatCurrent = numMatIndex; // current number value of the left pixel
     
@@ -76,14 +66,14 @@ int main(int argv, char** argc) {
     }
     
     /* for loops iterating though every pixel and getting the RGB values. */
-    for(int rows = 0; rows < left.rows; rows++){
+    for(int rows = 0; rows < output.rows; rows++){
         //numMatIndex = 0;
-        for(int cols = 0; cols < left.cols; cols++){
-            cout << "looping through every row col in left. rows: " << rows << ", cols: " << cols << "\n";
+        for(int cols = 0; cols < output.cols; cols++){
+            
             Vec3b leftIntensity = left.at<Vec3b>(rows,cols);
-            float B = leftIntensity.val[0]/5; // blue, green & red values
-            float G = leftIntensity.val[1]/5; // divided by 5 so colour doesn't have to be exactly the same
-            float R = leftIntensity.val[2]/5; // just close enough
+            float B = leftIntensity.val[0]/bucketSize; // blue, green & red values
+            float G = leftIntensity.val[1]/bucketSize; // divided by 5 so colour doesn't have to be exactly the same
+            float R = leftIntensity.val[2]/bucketSize; // just close enough
             int i = cols; // images start in same position
             
             /* deals with left pixel value. if not 0, keep using that value, otherwise give it a new value */
@@ -92,22 +82,20 @@ int main(int argv, char** argc) {
             } else {
                 numMatIndex++; // if it is 0 the colour hasn't been seen yet, so give it a new colour value.
                 numMatCurrent = numMatIndex; // the current colour is now this new value
-                numberMatrix[rows][cols] = numMatIndex;
+                numberMatrix[rows][cols] = numMatCurrent;
             }
-            
-            
             /* goes along the row until eye width apart comparing each pixel in the right image to the original pixel in the left image */
             while(i < cols + eyeWidth && i < right.cols){
                 Vec3b rightIntensity = right.at<Vec3b>(rows,i);
-                float Bcompare = (rightIntensity.val[0])/5;
-                float Gcompare = (rightIntensity.val[1])/5;
-                float Rcompare = (rightIntensity.val[2])/5 ;
+                float Bcompare = (rightIntensity.val[0])/bucketSize;
+                float Gcompare = (rightIntensity.val[1])/bucketSize;
+                float Rcompare = (rightIntensity.val[2])/bucketSize;
                 
                 
                 if(B == Bcompare && G == Gcompare && R == Rcompare){ // if the pixels are the same(ish)
-                    count++;
                     /* if pixel hasn't been given a value yet then give it the current value.
                      if pixel has a value that is different, replace every pixel with that value (including the current pixel) with the current value */
+                    count++;
                     if(numberMatrix[rows][i] == 0){
                         numberMatrix[rows][i] = numMatCurrent;
                     } else {
@@ -126,36 +114,35 @@ int main(int argv, char** argc) {
             }
         }
     }
+    uchar colour[numMatIndex]; //an array of colours
+    bool colourCheck[numMatIndex]; //array checks whether a number has been given a colour yet
+    int colourIndex = 0; //current assigned value being dealt with
     
-    cout << "left columns: " << left.cols << "\n";
-    cout << "left rows: " << left.rows << "\n";
-    cout << "output columns: " << output.cols << "\n";
-    cout << "output rows: " << output.rows << "\n";
-    
-    Vec3b colour[numMatIndex];
-    long colourIndex = 0;
-    Vec3b empty = {0,0,0};
     /* look at every item in the numbered matrix, every pixel with the same number gets given same grey/colour pixel
      if the number is 0 it is given a random greyscale/colour pixel */
+    seed();
     for(int rows = 0; rows < output.rows-1; rows++){
         for(int cols = 0; cols < output.cols-1; cols++){
             if(numberMatrix[rows][cols] == 0){
-                output.at<Vec3b>(rows,cols) = randomPixelColour();
+                output.at<Vec3b>(rows,cols) = randomPixelGrey(); //give pixel random colour
             } else {
                 colourIndex = numberMatrix[rows][cols];
-                if(colour[colourIndex] == empty){
-                colour[colourIndex] = randomPixelColour();
-            // same seed gives same pixel.
+                if (!colourCheck[colourIndex]) { //if the number hasn't been assigned a colour yet
+                    colour[colourIndex] = randomPixelGrey(); // assign it a colour
+                    colourCheck[colourIndex] = true; // track that it has been given a colour
                 }
-                output.at<Vec3b>(rows,cols) = colour[colourIndex];
-                
+                output.at<uchar>(rows,cols) = colour[colourIndex]; // output pixel gets that colour
+            }
                 
             }
         }
-    }
+    
+    
+    
+    
      /*end timer */
     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    std::cout<<"printf: "<< duration <<'\n';
+    cout<< "Took: " << duration << " seconds to process " << count << " identical pairs of pixels";
     
     /* Deals with showing the images and their position on the monitor */
     namedWindow("Output", WINDOW_FREERATIO);
