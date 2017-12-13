3fd48588ab59e58a021bad284e8f2b9cd5fa469e
Ryan Spear <rspear@oucs1357.otago.ac.nz>
2017-12-13 15:33:10 +1300

    show output

--- pleaseWork/main.cpp
+++ pleaseWork/main.cpp
@@ -18,17 +18,18 @@ using namespace std;
 
 
 int main(int argv, char** argc) {
+ 
     Mat left = imread("left.jpg");
     Mat right = imread("right.jpg");
     int bucketSize = 4;
-    
+
     autoStereo gram(left, right, bucketSize);
-    
+
     gram.fillNumberMatrix(left, right, bucketSize, gram.getNumberMatrix(), gram.getHeight());
-    
+
     gram.produceOutput(gram.getNumberMatrix(), gram.getOutput(), gram.getNumMatIndex(), gram.getHeight());
-    
-    
+
+    gram.showOutput(gram.getOutput(), left, right);
     
 }
 
